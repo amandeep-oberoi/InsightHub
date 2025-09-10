f8f125293e8f884f881c7599d6203d2d6f578458
amandeep-oberoi <amandeepkauroberoi856@gmail.com>
2025-09-10 10:25:42 +0000

    CRUD as transactions

--- srv/mysimplesrv.cds
+++ srv/mysimplesrv.cds
@@ -3,10 +3,15 @@ using {InsightHub.hrMs as hrMs} from '../db/Students';
 
 service mysrvdemo {
     @readonly
-    entity GetStudent    as projection on hrMs.Students;
+    entity GetStudent as projection on hrMs.Students;
 
     @updateOnly
     entity UpdateStudent as projection on hrMs.Students;
+
+    @insertonly
+    entity CreateStudent as projection on hrMs.Students;
+
+    action deleteStudent(email:String) returns Boolean;
 
     function myfoobar(firstName: String, lastName: String) returns String;
     function myfoobar2() returns String;
--- srv/mysimplesrv.js
+++ srv/mysimplesrv.js
@@ -1,6 +1,9 @@
 //const mysrvdemo = function(srv){
 
+const { error } = require("@sap/cds");
 const cds = require ("@sap/cds");
+const { trace } = require("@sap/cds/lib/srv/middlewares");
+const { response } = require("express");
 const {Students} = cds.entities("InsightHub.hrMs");
 
 
@@ -27,22 +30,93 @@ const mysrvdemo = (srv)=>{
 
       let name = req.data.firstName;
       let emailID = req.data.email;
+      // Transaction utilizing the handler "req" object
+      const transaction = cds.transaction(req);
+      const{UPDATE, SELECT} = cds.ql;
+      const updatedRows = await transaction.run(
+          UPDATE(Students)
+          .set({ firstName: name }).where({email:emailID})
+        )
+        if(updatedRows === 0){
+          req.error(error, "Error updating rows");
+        }
 
-      let result = await UPDATE(Students).set({
-        firstName: name
+        const response = await transaction.run(
+          SELECT.one.from(Students).where({email:emailID})
+        )
+
+      // creating a new transaction object i.e., not utilizing the "req" from API call
+
+      // const result = cds.transaction(async txn => {
+      //   const{UPDATE, SELECT} = cds.ql;
+      //   const updatedRows = await txn.run(
+      //     UPDATE(Students)
+      //     .set({ firstName: name }).where({email:emailID})
+      //   )
+      //   if(updatedRows === 0){
+      //     req.error(error, "Error updating rows");
+      //   }
+
+      //   const response = await txn.run(
+      //     SELECT.one.from(Students).where({email:emailID})
+      //   )
+      //   return response;
+      // });
+
+
+      // Update without using transaction 
+      // let result = await UPDATE(Students).set({
+      //   firstName: name
+      // })
+      // .where({
+      //   email:emailID
+      // });
+
+      return response;
+    }),
+
+    srv.on("CREATE","CreateStudent", async(req,res)=>{
+      const result = cds.transaction(async txn =>{
+        const{INSERT,SELECT} = cds.ql;
+         const InsertedRows = await txn.run(
+        INSERT.into(Students).entries(req.data)
+        )
+
+        if (InsertedRows ===0){
+          req.error(error,"Error Inserting Student")
+        }
+
+        const response = await txn.run(
+          SELECT.from(Students).where({email:req.data.email})
+        )
+        return response;
       })
-      .where({
-        email:emailID
-      })
-      return req.data;
+      return result;
+      
     })
-}
 
+
+    srv.on("deleteStudent", async (req, res) => {
+      const txn = cds.transaction(req);
+      const {DELETE} = cds.ql;
+      const deletedRows = await txn.run(
+        DELETE.from(Students).where({email:req.data.email})
+      )
+      if(deletedRows > 0){
+        return true;
+      }
+      return false;
+
+    })
+    
+}
 module.exports = mysrvdemo
 
 
 /*module.exports =(srv) =>{
     
 }*/
+
+// Create a Student and fetch
 
 
