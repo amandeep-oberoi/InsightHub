ec6638aae91f6ff0c66a9c5cd7c6ae881c8d9021
amandeep-oberoi <amandeepkauroberoi856@gmail.com>
2025-09-13 11:53:29 +0000

    Admin Dashboard UI

--- app/shopverse/webapp/controller/Home.controller.js
+++ app/shopverse/webapp/controller/Home.controller.js
@@ -1,10 +1,39 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], (Controller,JSONModel) => {
     "use strict";
 
     return Controller.extend("shopverse.controller.Home", {
         onInit() {
+            let aCategories =[
+                {
+                key: "Products",
+                value: "Products"
+                },
+                  {
+                key: "Categories",
+                value: "Categories"
+                },
+                  {
+                key: "Customers",
+                value: "Customers"
+                },
+                  {
+                key: "Orders",
+                value: "Orders"
+                },
+                 {
+                key: "Suppliers",
+                value: "Suppliers"
+                }
+        ]
+
+    var oCategoriesModel = new JSONModel(aCategories);
+		this.getView().setModel(oCategoriesModel,"categoryModel");
         }
+
+        	
+
     });
 });
